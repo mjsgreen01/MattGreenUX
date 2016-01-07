34b065bdbde962bb9c41e4882af9afc00d79df66
mjsgreen01 <mjsgreen01@gmail.com>
2016-01-06 19:52:15 -0500

    [cleanup] serving static assets in server.js

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ var express = require('express');
 var app = express();
 var port = 3006;
 
-// app.use('/js', express.static(__dirname + '/js'));
-// app.use('/dist', express.static(__dirname + '/../dist'));
-// app.use('/css', express.static(__dirname + '/css'));
-// app.use('/partials', express.static(__dirname + '/partials'));
-app.use(express.static(__dirname + '/'));
-app.use(express.static(__dirname + '/app'));
 
-app.use('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: __dirname+'/app' });
-});
+app.use(express.static(__dirname + '/app'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+
 
 console.log('listening on port '+port);
 app.listen(port); //the port you want to use
