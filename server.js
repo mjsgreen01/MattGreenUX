a5e10e9cabb261840003c6447929f4982793fb42
mjsgreen01 <mjsgreen01@gmail.com>
2016-01-13 20:39:57 -0500

    [refactor] more accurate html5mode support

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var port = 3006;
 app.use(express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
+app.use(['/projects', '/psal'], function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname+'/app' });
+});
+
 
 
 console.log('listening on port '+port);
