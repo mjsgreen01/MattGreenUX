1389f32ed342a2c9726837ddab7c2e61b5116442
mjsgreen01 <mjsgreen01@gmail.com>
2016-01-20 19:56:28 -0500

    [fix] remove ensureDomain func

--- dist/server.js
+++ dist/server.js
@@ -5,13 +5,13 @@ const port = process.env.PORT || 3000;
 const domain =  process.env.DOMAIN;
 
 function ensureDomain(req, res, next) {
-  if (!domain || req.hostname === domain) {
-    // OK, continue
-    return next();
-  };
+  // if (!domain || req.hostname === domain) {
+  //   // OK, continue
+  //   return next();
+  // };
 
-  // handle port numbers if you need non defaults
-  res.redirect(`http://${domain}${req.url}`);
+  // // handle port numbers if you need non defaults
+  // res.redirect(`http://${domain}${req.url}`);
 };
 
 const app = express();
