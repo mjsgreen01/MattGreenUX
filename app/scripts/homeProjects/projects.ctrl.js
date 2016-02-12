692863f4b4861fd8e71cabfa9cf90e7385d4c67c
mjsgreen01 <mjsgreen01@gmail.com>
2016-02-11 20:49:40 -0500

    [dev] add social-med. links

--- app/scripts/homeProjects/projects.ctrl.js
+++ app/scripts/homeProjects/projects.ctrl.js
@@ -7,6 +7,7 @@ angular.module('mattGreenUX')
   vm.projectsData = [];
 
   ProjectsFactory.getData().then(function(){
+    // this should be done in the service
     angular.forEach(ProjectsFactory.projectsOrder, function (alias) {
       vm.projectsData.push( ProjectsFactory.projectsData[alias] );
     });
