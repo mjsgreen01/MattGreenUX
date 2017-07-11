4291b63b6351b48f117e6b83a5c5b333dfb3d787
Matt Green <mattgreenux@gmail.com>
2017-07-11 19:01:29 -0400

    fix immigration reload issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3006;
 app.use(express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-app.use(['/projects', '/hunter', '/psal', '/erb', '/home', '/immig'], function(req, res, next) {
+app.use(['/projects', '/hunter', '/psal', '/erb', '/home', '/immigration'], function(req, res, next) {
     // Just send the index.html for other files to support HTML5Mode
     res.sendFile('index.html', { root: __dirname+'/app' });
 });
